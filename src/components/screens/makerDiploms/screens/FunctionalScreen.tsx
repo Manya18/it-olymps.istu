b7e8cb784167e9b4ec928e561ca419d7699032cf
digischh <gavrilovadiana28@gmail.com>
2023-11-04 19:12:17 +0400

    загрузка и отображение картинки, выбор формата

--- src/components/screens/makerDiploms/screens/FunctionalScreen.tsx
+++ src/components/screens/makerDiploms/screens/FunctionalScreen.tsx
@@ -1,27 +1,53 @@
 import styles from "./FunctionalScreen.module.css"
 import useStore from "@/components/useStore";
-import { Input, InputLabel } from '@mui/material';
+import { Box, Button, FormControlLabel, Input, InputLabel, Stack, Switch } from '@mui/material';
+import { useState } from "react";
 
 
 const FunctionalScreen = ()  => {
 
-    const {setFontSize, fontSize, setFontColor, fontColor, setXpos, xpos, setYpos, ypos} = useStore();
+    const {setFontSize, fontSize, setFontColor, fontColor, setXpos, xpos, setYpos, ypos, setNameImage, nameImage} = useStore();
+    const [checked, setChecked] = useState(false);
+    let wid = 40; // для горизонтального
+    function readFile(e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+              reader.onload = (function (file) {
+          return function (e) {
+            var r = e.target;
+            setNameImage(r.result);
+          };
+        })(file);
+        // получает URL файла
+        reader.readAsDataURL(file);
+    }
+    // переключаемся на горизонтальную раскладку
+    if (checked) wid = 80;
     return(
-        <div >
-            <InputLabel style={{fontSize: '5px'}}> Font size </InputLabel> 
-            <Input id="input" value={fontSize} type="number" onChange={(e)=> setFontSize(e.target.value)}/>
+    <>
+        <Stack direction="row">
+            <Stack direction="column" className={styles.screen}>
+                <InputLabel> Font size </InputLabel>
+                <Input id="input" value={fontSize} type="number" onChange={(e) => setFontSize(e.target.value)} />
 
-            <InputLabel style={{fontSize: '5px'}}> Font color </InputLabel> 
-            <Input id="input" value={fontColor} type="string" onChange={(e)=> setFontColor(e.target.value)}/>
+                <InputLabel> Font color </InputLabel>
+                <Input id="input" value={fontColor} type="string" onChange={(e) => setFontColor(e.target.value)} />
 
-            <InputLabel style={{fontSize: '5px'}}> X position </InputLabel> 
-            <Input id="input" value={xpos} type="number" onChange={(e)=> setXpos(e.target.value)}/>
+                <InputLabel> X position </InputLabel>
+                <Input id="input" value={xpos} type="number" onChange={(e) => setXpos(e.target.value)} />
 
-            <InputLabel style={{fontSize: '5px'}}> Y position </InputLabel> 
-            <Input id="input" value={ypos} type="number" onChange={(e)=> setYpos(e.target.value)}/>
+                <InputLabel> Y position </InputLabel>
+                <Input id="input" value={ypos} type="number" onChange={(e) => setYpos(e.target.value)} />
 
-            <div style={{fontSize:`${fontSize}px`, color: fontColor, position: 'fixed', left: `${xpos}px`, top: `${ypos}px`}}>tttt</div>
-        </div>
+                <FormControlLabel checked={checked} control={<Switch />} label="Вертикально/Горизонтально" onChange={(e) => setChecked(e.target.checked)} />
+                <InputLabel> Загрузить изображение </InputLabel>
+                <Input type="file" onChange={(e)=> readFile(e)}/>
+            </Stack>
+            <div className={styles.preview} style={{backgroundImage: `url(${nameImage})`, width: `${wid}vw`}}/>
+        </Stack>
+        <Button>dcs</Button>
+        <div style={{ fontSize: `${fontSize}px`, color: fontColor, position: 'fixed', left: `${xpos}vw`, top: `${ypos}vh` }}>tttt</div>
+    </>
     )
 }
 
