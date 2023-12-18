f0ff56ed299cf8f0c53f7f7c310739cd4dac1b18
Manya18 <61350844+Manya18@users.noreply.github.com>
2023-12-18 16:23:12 +0400

    create posts

--- src/components/feeds/Feed.tsx
+++ src/components/feeds/Feed.tsx
@@ -1,12 +1,14 @@
 import styles from "./NewsFeed.module.css";
 
 const NewsFeed = (data:any)  => {
-    console.log(data)
+    console.log(data.value.created_at)
+    const date = new Date(data.value.created_at)
+    const parsedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
     return (
         <div className={styles.main}>
             <b className={styles.headText}>{data.value.title}</b>
             <div className={styles.group}>
-                <p className={styles.date}>{data.value.creared_at}</p>
+                <p className={styles.date}>{parsedDate}</p>
                 <p className={styles.author}>{data.value.author.email}</p>
             </div>
             <p className={styles.textFeed}>{data.value.content}</p>
--- src/components/feeds/NewsFeed.tsx
+++ src/components/feeds/NewsFeed.tsx
@@ -1,17 +1,68 @@
-import { Input } from "@mui/material";
+import { Button, Input, TextField } from "@mui/material";
 import Feed from "./Feed";
 import styles from "./NewsFeed.module.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const NewsFeed = (postsData: any[])  => {
-
+    const [showButton, setShowCrButton] = useState(false);
+    const [titlePost, setTitlePost] = useState('');
+    const [contentPost, setContentPost] = useState('');
     console.log(Object.values(postsData)[0])
     const dataEv=Object.values(postsData)[0];
-   
+
+    const createPost =() => {
+        setShowCrButton(true); 
+        let elem = document.getElementById('createPost');
+        if(elem) elem.style.visibility = 'hidden'
+    }
+
+    async function publishPost() {
+        //изменяем отображение полей ввода
+        setShowCrButton(false);
+        //изменяем отображение кнопки создать запись
+        let elem = document.getElementById('createPost');
+        if(elem) elem.style.visibility = 'visible';
+        //пост запрос
+        console.log(titlePost, contentPost)
+        if(titlePost!='' && contentPost!='')
+        {
+            axios.post(`http://localhost:8080/api/v1/posts`, {
+                "title": `${titlePost}`,
+                "content": `${contentPost}`
+            })
+            .then(response => 
+                { 
+                    window.location.href='/';
+                    console.log(response)
+            })
+        }
+        
+    }
+
+    //это для получения куки
+    const [role, setRole] = useState('');
+
+    useEffect(() => {
+        const items = localStorage.getItem('role');
+        console.log(items)
+        if (items) {
+            setRole(items);
+        }
+    }, []);
+    console.log(role)
+
     return (
            <div>
-                <div className={styles.b}>Новостная лента:</div>
+                <div className={styles.newsLine}>
+                    <div className={styles.b}>Новостная лента:</div>
+                    {role == "admin" && <Button id="createPost" className={styles.buttonCreate} onClick={() => createPost()}>Создать запись</Button>}
+                </div>
+                {showButton && <div className={styles.createPost}>
+                    <TextField label="Заголовок" variant="outlined" multiline className={styles.field} onChange={(e) => setTitlePost(e.target.value)}></TextField>
+                    <TextField label="Текст" variant="outlined" multiline className={styles.field} onChange={(e) => setContentPost(e.target.value)}></TextField>
+                    <Button id="publush" className={styles.publish} onClick={() => publishPost()}>Опубликовать</Button>
+                    </div>}
                 {dataEv && dataEv.map((data: any) => 
                     <Feed value={data}/>
                 )}
