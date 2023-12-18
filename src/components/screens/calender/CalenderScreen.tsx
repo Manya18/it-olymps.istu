7c5941f14adac35e7927fc8dbb888a5dd134727b
Manya18 <61350844+Manya18@users.noreply.github.com>
2023-12-18 21:26:43 +0400

    add calender

--- src/components/screens/calender/CalenderScreen.tsx
+++ src/components/screens/calender/CalenderScreen.tsx
@@ -1,19 +1,32 @@
+import axios from "axios";
 import styles from "./CalenderScreen.module.css"
+import { useEffect, useState } from "react";
+import Event from "../home/Event";
 
 const CalenderScreen = ()  => {
-    const data = [
-    {
-        alt: "Иннотех",
-        src: "/innotech.png", 
-        link: "https://inno.tech/ru/"
-    },
-]
+    const [eventData, setEventData] = useState();
+
+    const getEventData = () => {
+        axios.get('http://localhost:8080/api/v1/events?page=0&onPage=10')
+        .then((response) => {
+            setEventData(response.data);
+            console.log(Object.values(response.data))
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    } 
+    useEffect(() => {
+        getEventData();
+    }, [])
+
     return (
-        <div className={styles.footer}>
-            <b>Календарь мероприятий</b>
-            <p>Сентябрь</p>
-            <div>
-                
+        <div className={styles.main}>
+            <div className={styles.headText}>Календарь мероприятий</div>
+            <div className={styles.blocks}>
+            {eventData && eventData.map((eventD:any) =>
+                <Event value={eventD}/>)
+            }
             </div>
         </div>
     )
